a1dd62f2b0475c81421521cac03c7fb173999e02
Andres <andres-a23@live.com>
2020-06-18 00:27:15 -0400

    completed algo 2

--- algos.js
+++ algos.js
@@ -1,13 +1,13 @@
+// Algo #1
 // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 // Return the running sum of nums.
 // Constraints: (1 <= nums.length <= 1000 - 10) ^ (6 <= nums[i] <= 10) ^ 6;
 
 // ----- pseudocode -----
-// create new array pointer
 // iterate though array
-// identify base case. Array reaches length
-// use recursion to add index ascending through array
-// add each result to new array pointer
+// Start at index 1 of array
+// add each result to last index number by subtracting one from [i]
+// return array
 
 const runningSum = (nums) => {
   for (let i = 1; i < nums.length; i++) {
@@ -15,17 +15,39 @@ const runningSum = (nums) => {
   }
   return nums;
 };
-console.log(runningSum([5, 2, 3, 4, 5]));
 
-const runningSum = (nums) => {
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] == 0) {
-      return
-    } else {
-      nums[i] += nums[i - 1]
-    }
+// console.log(runningSum([5, 2, 3, 4, 5]));
+
+// const runningSum = (nums) => {
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] == nums[0]) {
+//       continue
+//     } else {
+//       nums[i] += nums[i - 1]
+//     }
+//   }
+//   return nums;
+// };
+
+// console.log(runningSum([5, 2, 3, 4, 5]));
+
+
+// Algo #2
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn]
+
+// ----- pseudocode -----
+// Iterate through array
+
+
+const shuffle = (nums, n) => {
+  const newArray = [];
+
+  for (let i = 0; i < n; i++) {
+    newArray.push(nums[i]);
+    newArray.push(nums[i + n])
   }
-  return nums;
-};
-console.log(runningSum([5, 2, 3, 4, 5]));
+  return newArray;
+}
 
+console.log(shuffle([2, 5, 1, 3, 4, 7], 3));
