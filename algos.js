c80d03a2db4ae94c0b429b0a20a3f49c8fd68471
Andres <andres-a23@live.com>
2020-06-19 23:22:58 -0400

    Finished algo day 3

--- algos.js
+++ algos.js
@@ -50,4 +50,29 @@ const shuffle = (nums, n) => {
   return newArray;
 }
 
-console.log(shuffle([2, 5, 1, 3, 4, 7], 3));
+// console.log(shuffle([2, 5, 1, 3, 4, 7], 3));
+
+
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+// That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+// Return the answer in an array.
+
+// ----- pseudocode -----
+// Iterate through array 
+
+const smallerNumbersThanCurrent = (nums) => {
+  let newArray = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        count++
+      }
+    }
+    newArray[i]= count
+  }
+  return newArray
+  }
+
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
