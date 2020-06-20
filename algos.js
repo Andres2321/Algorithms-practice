c4148536728fd5f6721e3f90dbc09f1c5bc59870
Andres <andres-a23@live.com>
2020-06-20 10:35:09 -0400

    algorithm 3

--- algos.js
+++ algos.js
@@ -31,7 +31,6 @@ const runningSum = (nums) => {
 
 // console.log(runningSum([5, 2, 3, 4, 5]));
 
-
 // Algo #2
 // Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 // Return the array in the form [x1,y1,x2,y2,...,xn,yn]
@@ -39,26 +38,24 @@ const runningSum = (nums) => {
 // ----- pseudocode -----
 // Iterate through array
 
-
 const shuffle = (nums, n) => {
   const newArray = [];
 
   for (let i = 0; i < n; i++) {
     newArray.push(nums[i]);
-    newArray.push(nums[i + n])
+    newArray.push(nums[i + n]);
   }
   return newArray;
-}
+};
 
 // console.log(shuffle([2, 5, 1, 3, 4, 7], 3));
-
 
 // Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
 // That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
 // Return the answer in an array.
 
 // ----- pseudocode -----
-// Iterate through array 
+// Iterate through array
 
 // const smallerNumbersThanCurrent = (nums) => {
 //   let newArray = [];
@@ -77,13 +74,13 @@ const shuffle = (nums, n) => {
 
 // console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
 
-var smallerNumbersThanCurrent = function (nums) {
-  const sortedNums = [...nums].sort((a, b) => a - b);
-console.log(sortedNums)
-  for (let i = 0; i < nums.length; i++){
-    nums[i] = sortedNums.indexOf(nums[i])
-  };
-  return nums;
-};
+// var smallerNumbersThanCurrent = function (nums) {
+//   const sortedNums = [...nums].sort((a, b) => a - b);
 
-console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
+//   for (let i = 0; i < nums.length; i++) {
+//     nums[i] = sortedNums.indexOf(nums[i]);
+//   }
+//   return nums;
+// };
+
+// console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
