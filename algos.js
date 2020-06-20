f78d02826036caf45a27c3e4c11c630253239e70
Andres <andres-a23@live.com>
2020-06-20 12:35:37 -0400

    finishd algo 5

--- algos.js
+++ algos.js
@@ -29,7 +29,6 @@
 // };
 // console.log(runningSum([5, 2, 3, 4, 5]));
 
-
 // ===== Algo #2 =====
 // Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 // Return the array in the form [x1,y1,x2,y2,...,xn,yn]
@@ -47,7 +46,6 @@
 //   return newArray;
 // };
 // console.log(shuffle([2, 5, 1, 3, 4, 7], 3));
-
 
 // ===== Algo #3 =====
 // Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
@@ -73,10 +71,10 @@
 //   return newArray
 //   }
 
-  // ---- alternative solution ---
-  // sort array from smallest to largest
-  // finding index of array 
-  // return array
+// ---- alternative solution ---
+// sort array from smallest to largest
+// finding index of array
+// return array
 
 // console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
 
@@ -91,7 +89,6 @@
 
 // console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
 
-
 // ===== Algo #4 =====
 // Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
 // For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number
@@ -102,18 +99,43 @@
 // iterate through array and compare to max variable
 // return true or false is condition met
 
-const kidsWithCandies = (candies, extraCandies) => {
-  const maxCandy = Math.max(...candies)
-  const result = []
-  for (let i = 0; i < candies.length; i++) {
-    if (candies[i] === maxCandy) {
-      result.push(true)
-    } else if (candies[i] < maxCandy && candies[i] + extraCandies >= maxCandy) {
-      result.push(true);
-    } else {
-      result.push(false);
-    }
-  } 
-  return result;
-}
-console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
+// const kidsWithCandies = (candies, extraCandies) => {
+//   const maxCandy = Math.max(...candies)
+//   const result = []
+//   for (let i = 0; i < candies.length; i++) {
+//     if (candies[i] === maxCandy) {
+//       result.push(true)
+//     } else if (candies[i] < maxCandy && candies[i] + extraCandies >= maxCandy) {
+//       result.push(true);
+//     } else {
+//       result.push(false);
+//     }
+//   }
+//   return result;
+// }
+// console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
+
+// ===== Algo #5 =====
+// We are given a list nums of integers representing a list compressed with run-length encoding.
+// Consider each adjacent pair of elements[freq, val] = [nums[2 * i], nums[2 * i + 1]](with i >= 0).
+// For each such pair, there are freq elements with value val concatenated in a sublist.
+// Concatenate all the sublists from left to right to generate the decompressed list.
+// Return the decompressed list.
+
+// ----- pseudocode -----
+// divide array into odds and even index
+// odd index are frequency
+// even index are value
+
+const decompressRLElist = (nums) => {
+  const res = [];
+  for (let i = 0; i < nums.length; i += 2) {
+    const size = nums[i];
+    const val = nums[i + 1];
+    const arr = new Array(size).fill(val);
+    res.push(...arr);
+  }
+  return res;
+};
+
+console.log(decompressRLElist([1, 2, 3, 4]));
